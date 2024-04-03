adae1cf49d8e66b9bb24868fa7473da1bc8a4d6c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-03 09:14:20 -0400

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | My Acme App',
+    default: 'Acme Dashboard',
+  },
+  description: 'My app description',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
